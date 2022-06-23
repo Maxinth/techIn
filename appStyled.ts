ca25c40eed662f92930fb113adeadf0618497039
maxinth <tunde.j.adeleye@gmail.com>
2022-06-23 10:59:16 +0100

    fixed page overlow issue

--- appStyled.ts
+++ appStyled.ts
@@ -6,7 +6,23 @@ const GlobalStyle = createGlobalStyle`
   * {
     margin: 0;
     padding: 0;
+    box-sizing: border-box;
     font-family: Inter;
+  }
+
+  html{
+    width: 100%;
+   
+  }
+  body{
+    width: 100%;
+    max-width: 528px;
+    margin: 0 auto;
+  }
+
+  #__next{
+    width: 100%;  
+     
   }
 `;
 
--- components/Form/styled.ts
+++ components/Form/styled.ts
@@ -1,21 +1,47 @@
 import styled from "styled-components";
 
-const InputBox = styled.input``;
+const InputBox = styled.input`
+  /* max-width: 528px; */
+  max-height: 52px;
+  padding: 18px 20px 19px 20px;
+  background-color: #ffffff;
+  border: 1px solid #eaeced;
+  border-radius: 7px;
+  width: 100%;
+`;
 const Form = styled.form`
-  max-width: 528px;
   margin: 0 auto;
-  border: 1px solid red;
+  padding: 0rem 4rem;
+  margin-top: 158px;
+  max-width: 528px;
+  width: 100%;
+  /* overflow: hidden; */
 `;
 
 const Heading = styled.h1`
   font-size: 44px;
   line-height: 53.52px;
   letter-spacing: -5%;
-  font-weight: 700px;
+  font-weight: 700;
+  margin: 0 auto;
+  margin-bottom: 48px;
+  /* max-width: 528px; */
 `;
-const Label = styled.label``;
+const Label = styled.label`
+  font-size: 14px;
+  line-height: 17px;
+  margin-bottom: 12px;
+  font-weight: 400;
+`;
 const FieldBox = styled.div`
   width: 100%;
+  /* max-width: 528px; */
+  /* margin: 0 auto; */
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  flex-wrap: wrap;
 `;
 
 export { InputBox, Form, Label, FieldBox, Heading };
